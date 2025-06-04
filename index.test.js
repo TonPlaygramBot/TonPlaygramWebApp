a43545b85cee1a481a19c3de1e270c4b3d5421ac
turdjali <164725460+turdjali@users.noreply.github.com>
2025-06-04 21:33:43 +0200

    Fix webapp paths and tests

--- index.test.js
+++ index.test.js
@@ -1,7 +1,10 @@
 const fs = require('fs');
 const path = require('path');
+const { JSDOM } = require('jsdom');
 
 test('index.html contains root div', () => {
   const html = fs.readFileSync(path.join(__dirname, 'index.html'), 'utf8');
-  expect(html).toMatch(/<div id="root"><\/div>/);
+  const dom = new JSDOM(html);
+  const root = dom.window.document.getElementById('root');
+  expect(root).not.toBeNull();
 });
