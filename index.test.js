73cd6616a990d7babf27d26ededd9845ca69faea
turdjali <164725460+turdjali@users.noreply.github.com>
2025-06-05 09:34:44 +0200

    Delete index.test.js

--- index.test.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-
-test('index.html contains root div', () => {
-  const html = fs.readFileSync(path.join(__dirname, 'index.html'), 'utf8');
-  expect(html).toMatch(/<div id="root"><\/div>/);
-});
