bc50a3ee1658f4fdc31bbfce116b706ffb3562ef
turdjali <164725460+turdjali@users.noreply.github.com>
2025-06-05 09:33:40 +0200

    Delete src directory

--- src/pages/Tasks.jsx
+++
@@ -1,8 +0,0 @@
-export default function Tasks() {
-  return (
-    <div className="p-4">
-      <h2 className="text-xl font-bold mb-4">Tasks</h2>
-      <p>No tasks available.</p>
-    </div>
-  );
-}
