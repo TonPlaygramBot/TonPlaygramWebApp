0d433fc3f4de764749a57d84f73eb1b63c5fb889
nexhip <nexhipalimadhi8@gmail.com>
2025-06-07 18:36:26 +0200

    Refactoring

--- bot/bot.js
+++ bot/bot.js
@@ -12,7 +12,7 @@ import registerDice from './commands/games/dice.js';
 import registerLudo from './commands/games/ludo.js';
 import registerHorse from './commands/games/horse.js';
 import registerSnake from './commands/games/snake.js';
-
+const bot = new Telegraf(process.env.BOT_TOKEN);
 registerStart(bot);
 registerMine(bot);
 registerWatch(bot);
