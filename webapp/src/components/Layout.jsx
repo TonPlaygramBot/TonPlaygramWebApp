e43d7da5ccc6fcd16e22d8e80a263ce11a17fc80
turdjali <164725460+turdjali@users.noreply.github.com>
2025-06-10 09:50:22 +0200

    Display balance in mine command

--- webapp/src/components/Layout.jsx
+++ webapp/src/components/Layout.jsx
@@ -4,9 +4,9 @@ import Footer from './Footer.jsx';
 
 export default function Layout({ children }) {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen bg-gray-100 text-gray-900">
       <Navbar />
-      <main className="flex-grow p-4">
+      <main className="flex-grow container mx-auto p-4">
         {children}
       </main>
       <Footer />
