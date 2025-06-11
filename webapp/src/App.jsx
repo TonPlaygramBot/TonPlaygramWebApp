e87642307590204441dafcc796a1b9c8ac7184c7
turdjali <164725460+turdjali@users.noreply.github.com>
2025-06-11 17:09:05 +0200

    fix: remove debug breadcrumb reliably

--- webapp/src/App.jsx
+++ webapp/src/App.jsx
@@ -3,7 +3,6 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home.jsx';
 import Mining from './pages/Mining.jsx';
 import Wallet from './pages/Wallet.jsx';
-import WatchToEarn from './pages/WatchToEarn.jsx';
 import Tasks from './pages/Tasks.jsx';
 import Referral from './pages/Referral.jsx';
 import MyAccount from './pages/MyAccount.jsx';
@@ -24,7 +23,6 @@ export default function App() {
           <Route path="/games/ludo" element={<LudoGame />} />
           <Route path="/games/horse" element={<HorseRacing />} />
           <Route path="/games/snake" element={<SnakeLadders />} />
-          <Route path="/watch" element={<WatchToEarn />} />
           <Route path="/tasks" element={<Tasks />} />
           <Route path="/referral" element={<Referral />} />
           <Route path="/wallet" element={<Wallet />} />
