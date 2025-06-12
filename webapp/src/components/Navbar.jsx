44c8946b5818badb42d77774fe3dbe5724bbb87c
turdjali <164725460+turdjali@users.noreply.github.com>
2025-06-12 09:10:27 +0200

    Revert "Add chess board with chess.js"

--- webapp/src/components/Navbar.jsx
+++ webapp/src/components/Navbar.jsx
@@ -1,31 +1,19 @@
 import { Link } from 'react-router-dom';
 
 export default function Navbar() {
-
   return (
-
     <nav className="bg-black text-yellow-400 shadow border-b border-yellow-600">
-
       <div className="container mx-auto px-4 py-3 flex items-center">
-
         <div className="flex-1 space-x-4 text-sm">
-
           <Link className="hover:text-yellow-400" to="/">Home</Link>
-
           <Link className="hover:text-yellow-400" to="/mining">Mining</Link>
-
           <Link className="hover:text-yellow-400" to="/games/chess">Chess</Link>
-
           <Link className="hover:text-yellow-400" to="/tasks">Tasks</Link>
-
           <Link className="hover:text-yellow-400" to="/referral">Referral</Link>
-
           <Link className="hover:text-yellow-400" to="/wallet">Wallet</Link>
-
           <Link className="hover:text-yellow-400" to="/account">My Account</Link>
-
         </div>
-
       </div>
-
-    </
+    </nav>
+  );
+}
--- webapp/tailwind.config.js
+++ webapp/tailwind.config.js
@@ -1,35 +1,17 @@
 export default {
-
   content: [
-
     './index.html',
-
     './src/**/*.{js,jsx}'
-
   ],
-
   theme: {
-
     extend: {
-
       colors: {
-
-        dark: '#0b0f19',
-
         brand: {
-
           gold: '#f1c40f',
-
           black: '#000000'
-
         }
-
       }
-
     }
-
   },
-
   plugins: []
-
 };
