b4ed82ba695f4b282e553d38cd9bcf4dbd7d89f9
nexhip <nexhipalimadhi8@gmail.com>
2025-06-13 21:53:07 +0200

    Add logo on Home page

--- webapp/src/components/Branding.jsx
+++ webapp/src/components/Branding.jsx
@@ -4,9 +4,9 @@ export default function Branding() {
   return (
     <div className="text-center py-6 space-y-2">
       <img
-        src="/assets/TonPlaygramLogo.JPG"
+        src="/assets/TonPlayGramLogo.jpg"
         alt="TonPlaygram Logo"
-        className="mx-auto w-16 h-16"
+        className="mx-auto "
       />
     </div>
   );
