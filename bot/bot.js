60609db7ec7f39b9b5f660c8152e23907be93330
turdjali <164725460+turdjali@users.noreply.github.com>
2025-06-15 06:30:04 +0200

    Add Connect Four game with staking

--- bot/bot.js
+++ bot/bot.js
@@ -12,6 +12,7 @@ import registerDice from './commands/games/dice.js';
 import registerLudo from './commands/games/ludo.js';
 import registerHorse from './commands/games/horse.js';
 import registerSnake from './commands/games/snake.js';
+import registerConnectFour from './commands/games/connectfour.js';
 const bot = new Telegraf(process.env.BOT_TOKEN);
 registerStart(bot);
 registerMine(bot);
@@ -23,5 +24,6 @@ registerDice(bot);
 registerLudo(bot);
 registerHorse(bot);
 registerSnake(bot);
+registerConnectFour(bot);
 
 export default bot;
--- webapp/src/pages/Games.jsx
+++ webapp/src/pages/Games.jsx
@@ -7,6 +7,7 @@ export default function Games() {
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         <GameCard title="Chess" icon="/assets/icons/horse.svg" link="/games/chess" />
         <GameCard title="Snakes & Ladders" icon="/assets/icons/snake.svg" link="/games/snake" />
+        <GameCard title="Connect Four" icon="/assets/icons/connect4.svg" link="/games/connectfour" />
         <GameCard title="Dice Duel" icon="/assets/icons/dice.svg" link="/games/dice" />
       </div>
     </div>
