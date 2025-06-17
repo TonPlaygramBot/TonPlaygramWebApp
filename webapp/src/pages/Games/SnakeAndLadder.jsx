0948d3d051b180bbed550835982337a7a8a935e8
turdjali <164725460+turdjali@users.noreply.github.com>
2025-06-17 18:17:20 +0200

    Shift board slightly left

--- webapp/src/pages/Games/SnakeAndLadder.jsx
+++ webapp/src/pages/Games/SnakeAndLadder.jsx
@@ -209,7 +209,7 @@ function Board({
               "--cell-height": `${cellHeight}px`,
               "--board-width": `${cellWidth * COLS}px`,
               // Lower camera angle and zoom dynamically as the player moves
-              transform: `rotateX(${angle}deg) scale(${zoom})`,
+              transform: `translateX(-20px) rotateX(${angle}deg) scale(${zoom})`,
             }}
           >
             {tiles}
