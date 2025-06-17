a6b21fcb02a20955c7f8a6b664e88f5fe2f501b9
turdjali <164725460+turdjali@users.noreply.github.com>
2025-06-17 18:28:28 +0200

    Use cube token in Snake & Ladder

--- webapp/src/components/PlayerToken.jsx
+++ webapp/src/components/PlayerToken.jsx
@@ -4,18 +4,17 @@ export default function PlayerToken({ photoUrl, type = 'normal', color }) {
   const colorClass =
     type === 'ladder' ? 'token-green' : type === 'snake' ? 'token-red' : 'token-yellow';
   const style = color ? { '--side-color': color, '--border-color': color } : undefined;
+
   return (
-    <div className={`player-token ${colorClass}`} style={style}>
-      <img src={photoUrl} alt="player" className="token-top" />
-      <div className="hex-cylinder">
-        <div className="hex-side side-1" />
-        <div className="hex-side side-2" />
-        <div className="hex-side side-3" />
-        <div className="hex-side side-4" />
-        <div className="hex-side side-5" />
-        <div className="hex-side side-6" />
+    <div className={`token-cube ${colorClass}`} style={style}>
+      <div className="token-cube-inner">
+        <img src={photoUrl} alt="player" className="cube-face cube-top" />
+        <div className="cube-face cube-bottom" />
+        <div className="cube-face cube-front" />
+        <div className="cube-face cube-back" />
+        <div className="cube-face cube-right" />
+        <div className="cube-face cube-left" />
       </div>
-      <div className="token-base" />
     </div>
   );
 }
