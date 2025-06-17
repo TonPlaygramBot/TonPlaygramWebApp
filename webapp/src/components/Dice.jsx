de10b4f978b5be499215b3c934ed2898d9304363
turdjali <164725460+turdjali@users.noreply.github.com>
2025-06-17 10:31:01 +0200

    Reduce dice size in snake and ladder

--- webapp/src/components/Dice.jsx
+++ webapp/src/components/Dice.jsx
@@ -97,9 +97,9 @@ function DiceCube({
   }, [rolling, playSound]);
 
   return (
-    <div className="dice-container perspective-1000 w-24 h-24">
+    <div className="dice-container perspective-1000 w-20 h-20">
       <div
-        className={`dice-cube relative w-full h-full transition-transform duration-500 transform-style-preserve-3d ${
+        className={`dice-cube transition-transform duration-500 ${
           rolling ? "animate-roll" : ""
         }`}
         style={{ transform: orientation }}
