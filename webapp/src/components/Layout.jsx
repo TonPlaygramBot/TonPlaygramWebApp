6c881a68d39ab06869464ca33983553724c30478
turdjali <164725460+turdjali@users.noreply.github.com>
2025-06-17 14:02:04 +0200

    Hide navbar on game pages

--- webapp/src/components/Layout.jsx
+++ webapp/src/components/Layout.jsx
@@ -7,18 +7,24 @@ import Branding from "./Branding.jsx";
 export default function Layout({ children }) {
   const location = useLocation();
   const showBranding = !location.pathname.startsWith('/games');
+  const showNavbar = !(
+    location.pathname.startsWith('/games/') &&
+    !location.pathname.includes('/lobby')
+  );
 
   return (
     <div className="flex flex-col min-h-screen bg-background text-text relative">
-      <main className="flex-grow container mx-auto p-4 pb-24">
+      <main className={`flex-grow container mx-auto p-4 ${showNavbar ? 'pb-24' : ''}`.trim()}>
         {showBranding && <Branding />}
         {children}
       </main>
 
       {/* Fixed Bottom Navbar */}
-      <div className="fixed bottom-0 inset-x-0 z-50">
-        <Navbar />
-      </div>
+      {showNavbar && (
+        <div className="fixed bottom-0 inset-x-0 z-50">
+          <Navbar />
+        </div>
+      )}
 
       <Footer />
     </div>
