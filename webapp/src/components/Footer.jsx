5739cac51a489ea45d7c72411da8e56af30cde43
turdjali <164725460+turdjali@users.noreply.github.com>
2025-06-17 17:52:26 +0200

    Make footer background transparent

--- webapp/src/components/Footer.jsx
+++ webapp/src/components/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="bg-surface text-subtext text-sm border-t border-accent">
+    <footer className="bg-transparent text-subtext text-sm border-t border-accent">
       <div className="container mx-auto p-4 text-center">
         &copy; {new Date().getFullYear()} TonPlaygram
       </div>
