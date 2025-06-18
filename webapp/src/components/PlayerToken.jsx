093bf3938c4338b02aa406797817208f8785ae5a
turdjali <164725460+turdjali@users.noreply.github.com>
2025-06-18 08:26:27 +0200

    Revert "Show profile photo on token"

--- webapp/src/components/PlayerToken.jsx
+++ webapp/src/components/PlayerToken.jsx
@@ -8,16 +8,5 @@ export default function PlayerToken({ type = "normal", color, photoUrl }) {
     else if (type === "snake") tokenColor = "#fca5a5"; // red
     else tokenColor = "#fde047"; // yellow
   }
-  return (
-    <div className="relative">
-      <HexPrismToken color={tokenColor} photoUrl={photoUrl} />
-      {photoUrl && (
-        <img
-          src={photoUrl}
-          alt="token avatar"
-          className="absolute top-1/2 left-1/2 w-16 h-16 rounded-full -translate-x-1/2 -translate-y-1/2 pointer-events-none"
-        />
-      )}
-    </div>
-  );
+  return <HexPrismToken color={tokenColor} photoUrl={photoUrl} />;
 }
