ea858a8bd3b40c08171b3e147f7f04df159be00d
turdjali <164725460+turdjali@users.noreply.github.com>
2025-07-06 17:20:43 +0200

    Add 3D domino table styling

--- webapp/src/components/DominoBoard.jsx
+++ webapp/src/components/DominoBoard.jsx
@@ -3,10 +3,12 @@ import DominoPiece from './DominoPiece.jsx';
 
 export default function DominoBoard({ pieces = [] }) {
   return (
-    <div className="domino-board">
-      {pieces.map((p, i) => (
-        <DominoPiece key={i} left={p.left} right={p.right} />
-      ))}
+    <div className="domino-table">
+      <div className="domino-board">
+        {pieces.map((p, i) => (
+          <DominoPiece key={i} left={p.left} right={p.right} />
+        ))}
+      </div>
     </div>
   );
 }
