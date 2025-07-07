c9e4035b58a246461e763d3f89513a533837edb4
turdjali <164725460+turdjali@users.noreply.github.com>
2025-07-07 16:03:25 +0200

    Remove logo from advertisement modal

--- webapp/src/components/AdModal.tsx
+++ webapp/src/components/AdModal.tsx
@@ -55,7 +55,6 @@ export default function AdModal({ open, onComplete, onClose }: AdModalProps) {
             &times;
           </button>
         )}
-        <img  src="/assets/TonPlayGramLogo.jpg" alt="TonPlaygram Logo" className="w-10 h-10 mx-auto" />
         <h3 className="text-lg font-bold">Watch Ad</h3>
         <div
           id="adsgram-player"
