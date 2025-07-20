247f334a8c3127943bc4e69e0bd796271b97726a
turdjali <164725460+turdjali@users.noreply.github.com>
2025-07-20 08:05:49 +0200

    Remove TON/USDT staking contracts

--- webapp/src/utils/constants.js
+++ webapp/src/utils/constants.js
@@ -8,13 +8,6 @@ export const DEV_INFO = {
 // TON wallet used for store purchases and Adsgram payouts
 export const ADSGRAM_WALLET = 'UQDqDBiNU132j15Qka5EmSf37jCTLF-RdOlaQOXLHIJ5t-XT';
 
-// Address where players stake TON or USDT before each game
-export const STAKE_CONTRACT_ADDRESS =
-  'UQCm19A1xsUcH0_qsuFHJTljXI_DkEO6y6W9TAQ25TTCeFFS';
-
-// Developer wallet receiving 10% of the winnings
-export const DEV_STAKE_ADDRESS =
-  'UQAPwsGyKzA4MuBnCflTVwEcTLcGS9yV6okJWQGzO5VxVYD1';
-
 // Address of the Snake & Ladder smart contract used for TON bets
-export const SNAKE_CONTRACT_ADDRESS = STAKE_CONTRACT_ADDRESS;
+export const SNAKE_CONTRACT_ADDRESS =
+  'EQCXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
