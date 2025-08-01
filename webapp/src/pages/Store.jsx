733cce1ac50b1ed39be2a97d5220982785193d6e
turdjali <164725460+turdjali@users.noreply.github.com>
2025-08-01 21:18:32 +0200

    style(store): match chart card size

--- webapp/src/pages/Store.jsx
+++ webapp/src/pages/Store.jsx
@@ -19,7 +19,10 @@ export default function Store() {
           Address: {PTONTPC_LP_TOKEN.address}
         </p>
       </div>
-      <div id="dexscreener-embed" className="wide-card">
+      <div
+        id="dexscreener-embed"
+        className="relative bg-surface border border-border rounded-xl p-4 overflow-hidden wide-card"
+      >
         <iframe
           src="https://dexscreener.com/ton/EQBQ51T0Oo_iKUQvs2B0-MqAxnS_UZ3DEST-zJmQC7XYw0ix?embed=1&loadChartSettings=0&tabs=0&chartLeftToolbar=0&chartDefaultOnMobile=1&chartTheme=dark&theme=light&chartStyle=0&chartType=usd&interval=15"
           title="DexScreener"
