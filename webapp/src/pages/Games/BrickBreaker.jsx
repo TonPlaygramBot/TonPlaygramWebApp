5594968165f7b133e370e8ad7dbf54a40c4ef145
turdjali <164725460+turdjali@users.noreply.github.com>
2025-08-11 19:18:46 +0200

    Make Brick Breaker full-screen and improve paddle

--- webapp/src/pages/Games/BrickBreaker.jsx
+++ webapp/src/pages/Games/BrickBreaker.jsx
@@ -1,14 +1,12 @@
 import { useLocation } from 'react-router-dom';
-import useTelegramBackButton from '../../hooks/useTelegramBackButton.js';
 
 export default function BrickBreaker() {
-  useTelegramBackButton();
   const { search } = useLocation();
   return (
     <iframe
       src={`/brick-breaker.html${search}`}
       title="Brick Breaker Royale"
-      className="w-full h-screen border-0"
+      className="w-screen h-screen border-0"
     />
   );
 }
