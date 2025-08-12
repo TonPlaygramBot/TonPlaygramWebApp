81e730a97cc6d39eaa62b1f7a3a0be2f6bb72ce4
turdjali <164725460+turdjali@users.noreply.github.com>
2025-08-12 21:12:14 +0200

    Revert "docs: map routes and stub realtime layer"

--- realtime/games/snake.js
+++
@@ -1,9 +0,0 @@
-export const maxPlayers = 4;
-
-export function createState() {
-  return { scores: {} };
-}
-
-export function onInput(room, playerId, _payload) {
-  room.data.scores[playerId] = (room.data.scores[playerId] || 0) + 1;
-}
