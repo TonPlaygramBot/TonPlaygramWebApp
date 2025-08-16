715d63655f84ac9ab7ae175b9fe35aa454bef07c
turdjali <164725460+turdjali@users.noreply.github.com>
2025-08-16 16:35:34 +0200

    Revert "feat: update poll royale visuals"

--- webapp/src/components/HomeGamesCard.jsx
+++ webapp/src/components/HomeGamesCard.jsx
@@ -80,8 +80,8 @@ export default function HomeGamesCard() {
             to="/games/pollroyale/lobby"
             className="flex flex-col items-center space-y-1 border border-border rounded-lg p-2 flex-shrink-0 tetris-grid-bg"
           >
-            <img src="/assets/icons/64e79228-35e3-4fdc-b914-fca635a40220.webp" alt="" className="h-20 w-20" />
-            <h3 className="text-sm font-semibold text-center text-yellow-400">8 Ball Royale</h3>
+            <img src="/assets/icons/pool-royale.svg" alt="" className="h-20 w-20" />
+            <h3 className="text-sm font-semibold text-center text-yellow-400">8 Poll Royale</h3>
           </Link>
         </div>
       <Link
