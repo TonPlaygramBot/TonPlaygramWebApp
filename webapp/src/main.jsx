8bd33e4a30693e2d8e77be1852281e87247cd3c0
turdjali <164725460+turdjali@users.noreply.github.com>
2025-09-11 10:42:53 +0200

    Disable vertical swipe closing in Telegram WebApp

--- webapp/src/main.jsx
+++ webapp/src/main.jsx
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 
+// Prevent Telegram in-app browser swipe-down from closing the game
+if (window.Telegram?.WebApp?.disableVerticalSwipes) {
+  window.Telegram.WebApp.disableVerticalSwipes();
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
