7c24d429f31ff8ca3589ae69c2c652bc21e039aa
turdjali <164725460+turdjali@users.noreply.github.com>
2025-10-04 13:11:39 +0200

    Replace Pool Royale iframe with 3D variant scenes

--- webapp/src/pages/Games/PoolRoyale.jsx
+++ webapp/src/pages/Games/PoolRoyale.jsx
@@ -1,24 +1,21 @@
 import { useLocation } from 'react-router-dom';
 import useTelegramBackButton from '../../hooks/useTelegramBackButton.js';
+import PoolUK8 from './PoolUK8.jsx';
+import NineBall from './NineBall.jsx';
+import American8 from './American8.jsx';
+
+const VARIANT_COMPONENTS = {
+  uk: PoolUK8,
+  american: American8,
+  '9ball': NineBall
+};
 
 export default function PoolRoyale() {
   useTelegramBackButton();
   const { search } = useLocation();
   const params = new URLSearchParams(search);
-  const variant = params.get('variant') || 'uk';
-  params.set('variant', variant);
-  const type = params.get('type') || 'regular';
-  params.set('type', type);
-  const src = `/pool-royale.html?${params.toString()}`;
-  const title =
-    variant === '9ball'
-      ? '9-Ball'
-      : variant === 'american'
-        ? 'American Billiards'
-        : '8 Pool UK';
-  return (
-    <div className="relative w-full h-[100dvh]">
-      <iframe src={src} title={title} className="w-full h-full border-0" />
-    </div>
-  );
+  const variantKey = params.get('variant');
+  const VariantComponent = VARIANT_COMPONENTS[variantKey] || VARIANT_COMPONENTS.uk;
+
+  return <VariantComponent />;
 }
