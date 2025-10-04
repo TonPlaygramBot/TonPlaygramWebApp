b3918bc87d71f3ddf7eda39e15a692ebfec94de9
turdjali <164725460+turdjali@users.noreply.github.com>
2025-10-04 12:16:06 +0200

    feat: add 3D arena variants for Pool Royale

--- webapp/src/pages/Games/PoolRoyale.jsx
+++ webapp/src/pages/Games/PoolRoyale.jsx
@@ -1,5 +1,6 @@
 import { useLocation } from 'react-router-dom';
 import useTelegramBackButton from '../../hooks/useTelegramBackButton.js';
+import PoolRoyale3D from './PoolRoyale3D.jsx';
 
 export default function PoolRoyale() {
   useTelegramBackButton();
@@ -9,6 +10,8 @@ export default function PoolRoyale() {
   params.set('variant', variant);
   const type = params.get('type') || 'regular';
   params.set('type', type);
+  const engine = params.get('engine') || '3d';
+  params.set('engine', engine);
   const src = `/pool-royale.html?${params.toString()}`;
   const title =
     variant === '9ball'
@@ -16,9 +19,13 @@ export default function PoolRoyale() {
       : variant === 'american'
         ? 'American Billiards'
         : '8 Pool UK';
-  return (
-    <div className="relative w-full h-[100dvh]">
-      <iframe src={src} title={title} className="w-full h-full border-0" />
-    </div>
-  );
+  if (engine === 'legacy') {
+    return (
+      <div className="relative w-full h-[100dvh]">
+        <iframe src={src} title={title} className="w-full h-full border-0" />
+      </div>
+    );
+  }
+
+  return <PoolRoyale3D variant={variant} key={`${variant}-${type}`} />;
 }
