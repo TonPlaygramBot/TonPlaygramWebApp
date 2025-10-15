53febed4c7988d21e5fceae9cdb795825327eff6
turdjali <164725460+turdjali@users.noreply.github.com>
2025-10-15 20:28:10 +0200

    feat: add 3D arenas for poker and blackjack

--- webapp/src/pages/Games/TexasHoldem.jsx
+++ webapp/src/pages/Games/TexasHoldem.jsx
@@ -1,16 +1,13 @@
 import { useLocation } from 'react-router-dom';
 import useTelegramBackButton from '../../hooks/useTelegramBackButton.js';
+import TexasHoldemArena from './TexasHoldemArena.jsx';
 
 export default function TexasHoldem() {
   useTelegramBackButton();
   const { search } = useLocation();
   return (
     <div className="relative w-full h-screen">
-      <iframe
-        src={`/texas-holdem.html${search}`}
-        title="Texas Hold'em"
-        className="w-full h-full border-0"
-      />
+      <TexasHoldemArena search={search} />
     </div>
   );
 }
