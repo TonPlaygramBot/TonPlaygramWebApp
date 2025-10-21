de9cd611f8f40278c697d712f0e84902aebdc490
turdjali <164725460+turdjali@users.noreply.github.com>
2025-10-21 19:12:12 +0200

    Align Domino Royal 3D arena with poker experience

--- webapp/src/pages/Games/DominoRoyal.jsx
+++ webapp/src/pages/Games/DominoRoyal.jsx
@@ -1,8 +1,14 @@
 import { useLocation } from 'react-router-dom';
 
+import useTelegramBackButton from '../../hooks/useTelegramBackButton.js';
 import DominoRoyalArena from './DominoRoyalArena.jsx';
 
 export default function DominoRoyal() {
+  useTelegramBackButton();
   const { search } = useLocation();
-  return <DominoRoyalArena search={search} />;
+  return (
+    <div className="relative w-full h-screen">
+      <DominoRoyalArena search={search} />
+    </div>
+  );
 }
