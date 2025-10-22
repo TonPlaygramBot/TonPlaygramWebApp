ec687dc68c9d9fa169e44827180a3b9e0557dd67
turdjali <164725460+turdjali@users.noreply.github.com>
2025-10-22 15:13:13 +0200

    feat: replace free kick with 3d version

--- webapp/src/pages/Games/FreeKick.jsx
+++ webapp/src/pages/Games/FreeKick.jsx
@@ -1,16 +1,27 @@
 import { useLocation } from 'react-router-dom';
+import { useMemo } from 'react';
 import useTelegramBackButton from '../../hooks/useTelegramBackButton.js';
+import FreeKick3DGame from '../../components/FreeKick3DGame.jsx';
+
+function buildConfig(search) {
+  const params = new URLSearchParams(search);
+  const duration = params.get('duration');
+  const playerName =
+    params.get('name') || params.get('username') || params.get('player') || '';
+  return {
+    duration: duration ? Number(duration) : undefined,
+    playerName: playerName || undefined
+  };
+}
 
 export default function FreeKick() {
   useTelegramBackButton();
   const { search } = useLocation();
+  const config = useMemo(() => buildConfig(search), [search]);
+
   return (
-    <div className="relative w-full h-[100dvh]">
-      <iframe
-        src={`/free-kick.html${search}`}
-        title="Free Kick"
-        className="w-full h-full border-0"
-      />
+    <div className="relative h-[100dvh] w-full bg-[#07130f]">
+      <FreeKick3DGame config={config} />
     </div>
   );
 }
