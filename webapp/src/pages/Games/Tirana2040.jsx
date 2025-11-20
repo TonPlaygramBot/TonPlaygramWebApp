1c0aeea9a38f62360458805df00fd82e377bf5be
turdjali <164725460+turdjali@users.noreply.github.com>
2025-11-20 20:16:11 +0100

    Revert "Fix Tirana 2040 iframe path"

--- webapp/src/pages/Games/Tirana2040.jsx
+++ webapp/src/pages/Games/Tirana2040.jsx
@@ -5,12 +5,10 @@ export default function Tirana2040() {
   useTelegramBackButton();
   const { search } = useLocation();
 
-  const iframeSrc = `${import.meta.env.BASE_URL || '/'}tirana-2040.html${search || ''}`;
-
   return (
     <div className="relative w-full h-[100dvh]">
       <iframe
-        src={iframeSrc}
+        src={`/tirana-2040.html${search}`}
         title="Tirana 2040"
         className="w-full h-full border-0"
         allow="accelerometer; gyroscope; fullscreen"
