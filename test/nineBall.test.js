ee8b1b2528c52faf24eed1d22d92cd3c83f94a2b
turdjali <164725460+turdjali@users.noreply.github.com>
2025-12-01 08:22:29 +0100

    Restore previous American and nine-ball logic

--- test/nineBall.test.js
+++ test/nineBall.test.js
@@ -40,45 +40,6 @@ test('scratch gives opponent ball in hand', () => {
   assert.equal(res1.foul, true);
   assert.equal(res1.reason, 'scratch');
   assert.equal(res1.ballInHandNext, true);
+  assert.equal(res1.nextPlayer, 'B');
   assert.equal(game.state.currentPlayer, 'B');
-  const res2 = game.shotTaken({
-    contactOrder: [1],
-    potted: [],
-    cueOffTable: false,
-    placedFromHand: true
-  });
-  assert.equal(res2.foul, false);
-  assert.equal(res2.ballInHandNext, false);
-});
-
-test('foul still grants ball-in-hand and removes pocketed object balls', () => {
-  const game = new NineBall();
-  const res = game.shotTaken({
-    contactOrder: [],
-    potted: [2, 3],
-    cueOffTable: true,
-    placedFromHand: false
-  });
-  assert.equal(res.foul, true);
-  assert.equal(res.reason, 'no contact');
-  assert.equal(res.nextPlayer, 'B');
-  assert.equal(res.ballInHandNext, true);
-  assert.equal(game.state.ballsOnTable.has(2), false);
-  assert.equal(game.state.ballsOnTable.has(3), false);
-  assert.equal(game.state.ballsOnTable.has(9), true);
-});
-
-test('pocketing the nine on a foul respots it', () => {
-  const game = new NineBall();
-  const res = game.shotTaken({
-    contactOrder: [1],
-    potted: [1, 9, 0],
-    cueOffTable: true,
-    placedFromHand: false
-  });
-  assert.equal(res.foul, true);
-  assert.equal(res.reason, 'scratch');
-  assert.equal(res.ballInHandNext, true);
-  assert.equal(game.state.ballsOnTable.has(9), true);
-  assert.equal(game.state.ballsOnTable.has(1), false);
 });
