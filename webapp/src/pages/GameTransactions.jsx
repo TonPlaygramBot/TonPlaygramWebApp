8efacae61c4c5ac864442704e69aab7d00fdf9db
turdjali <164725460+turdjali@users.noreply.github.com>
2025-12-06 16:39:49 +0100

    Add battle royale football logic and AI

--- webapp/src/pages/GameTransactions.jsx
+++ webapp/src/pages/GameTransactions.jsx
@@ -11,7 +11,7 @@ const GAME_NAME_MAP = {
   texas: "Texas Hold'em",
   domino: 'Domino Royal 3D',
   blackjack: 'Black Jack Multiplayer',
-  freekick: 'Free Kick',
+  freekick: 'Football Battle Royale',
   murlan: 'Murlan Royale',
   tirana2040: 'Tirana 2040',
 };
