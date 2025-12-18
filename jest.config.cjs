d0c7a6662991bb957542679ce94045a58d3ad6a2
turdjali <164725460+turdjali@users.noreply.github.com>
2025-12-18 11:43:46 +0100

    Add PoolRoyaleRules tests and update Jest config

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,5 +1,22 @@
+/** @type {import('jest').Config} */
 module.exports = {
-  preset: 'ts-jest',
+  preset: 'ts-jest/presets/default-esm',
   testEnvironment: 'node',
-  roots: ['<rootDir>/tests'],
+  roots: ['<rootDir>/test'],
+  extensionsToTreatAsEsm: ['.ts', '.tsx'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1'
+  },
+  transform: {
+    '^.+\\.(ts|tsx|js)$': [
+      'ts-jest',
+      {
+        useESM: true,
+        tsconfig: {
+          allowImportingTsExtensions: true,
+          allowJs: true
+        }
+      }
+    ]
+  }
 };
