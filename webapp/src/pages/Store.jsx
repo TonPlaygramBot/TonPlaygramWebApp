11f119193dd9fa669d7f0f0ccc7a752dae91c80d
turdjali <164725460+turdjali@users.noreply.github.com>
2025-12-29 18:22:24 +0100

    Revert "Add Murlan furniture options to Chess Battle"

--- webapp/src/pages/Store.jsx
+++ webapp/src/pages/Store.jsx
@@ -146,7 +146,6 @@ const CHESS_TYPE_LABELS = {
   tableCloth: 'Table Cloth',
   tableBase: 'Table Base',
   chairColor: 'Chairs',
-  tables: 'Table Models',
   tableShape: 'Table Shape',
   sideColor: 'Piece Colors',
   boardTheme: 'Board Themes',
