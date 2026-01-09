11664833e675df945990e3dc486b67d5856eff68
turdjali <164725460+turdjali@users.noreply.github.com>
2026-01-09 15:49:47 +0100

    Revert "Add Murlan table finish option matching Pool Royale"

--- webapp/src/pages/Store.jsx
+++ webapp/src/pages/Store.jsx
@@ -175,7 +175,6 @@ const MURLAN_TYPE_LABELS = {
   cards: 'Card Themes',
   stools: 'Stools & Chairs',
   tables: 'Table Models',
-  tableFinish: 'Table Finishes',
   environmentHdri: 'HDR Environments'
 };
 
--- webapp/src/utils/murlanTable.js
+++ webapp/src/utils/murlanTable.js
@@ -20,10 +20,10 @@ const DEFAULT_TABLE_BASE_OPTION = Object.freeze({
 });
 
 const DEFAULT_TABLE_WOOD_OPTION = Object.freeze({
-  id: 'peelingPaintWeathered',
-  label: 'Wood Peeling Paint Weathered',
+  id: 'oakEstate',
+  label: 'Lis Estate',
   presetId: 'oak',
-  grainId: 'wood_peeling_paint_weathered'
+  grainId: 'estateBands'
 });
 
 export const DEFAULT_TABLE_CLOTH_OPTION = Object.freeze({
