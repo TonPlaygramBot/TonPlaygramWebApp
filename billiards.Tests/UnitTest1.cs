dec3388f079e3c8b3398ff1181392745a6c090af
turdjali <164725460+turdjali@users.noreply.github.com>
2026-01-21 21:18:21 -0800

    Adjust pocket cushions and pocketed ball roll

--- billiards.Tests/UnitTest1.cs
+++ billiards.Tests/UnitTest1.cs
@@ -129,7 +129,8 @@ public class PocketEdgeTests
         var balls = new List<BilliardsSolver.Ball> { ball };
         solver.Step(balls, 0.3);
         Assert.That(ball.Pocketed, Is.True);
-        Assert.That(balls, Is.Empty);
+        Assert.That(balls, Has.Count.EqualTo(1));
+        Assert.That(ball.Velocity.Length, Is.GreaterThanOrEqualTo(0));
     }
 }
 
