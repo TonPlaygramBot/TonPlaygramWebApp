44dc0be82e765381afbe7307c100810ad00ea4be
turdjali <164725460+turdjali@users.noreply.github.com>
2026-01-25 05:57:05 +0100

    Revert "Improve Pool Royale AI cue placement, audio & replay cue visibility; add modular Commentary + Chatterbox TTS system"

--- webapp/src/commentary/index.js
+++
@@ -1,4 +0,0 @@
-export { COMMENTARY_DB, COMMENTARY_EVENT_CATEGORY_MAP } from './commentaryDatabase.js';
-export { CommentaryManager } from './commentaryManager.js';
-export { ChatterboxTtsService } from './chatterboxTtsService.js';
-export { EVENT_PAYLOAD_SCHEMAS } from './eventSchemas.js';
