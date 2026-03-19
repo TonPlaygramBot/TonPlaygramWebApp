4d8950f580f44f3b5b9ba31cad4171f04bab5aba
turdjali <164725460+turdjali@users.noreply.github.com>
2026-03-19 17:16:06 +0100

    Revert "Separate and fix Pool Royale rulesets: American (race61), 9-ball, and BCA 8-ball"

--- lib/bcaEightBall.d.ts
+++
@@ -1,9 +0,0 @@
-export type BcaEightBallState = any;
-export type BcaEightBallShot = any;
-export type BcaEightBallResult = any;
-export class BcaEightBall {
-  constructor();
-  state: BcaEightBallState;
-  shotTaken(shot: BcaEightBallShot): BcaEightBallResult;
-}
-export default BcaEightBall;
