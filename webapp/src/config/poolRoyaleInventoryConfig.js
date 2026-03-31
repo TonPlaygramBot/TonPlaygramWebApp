db82dd4a87d97080fe3c8e93e5a6fc1b4005ac5d
turdjali <164725460+turdjali@users.noreply.github.com>
2026-03-31 12:06:23 +0200

    Revert "Align Murlan HDRI resolutions with Poly Haven mappings"

--- webapp/src/config/poolRoyaleInventoryConfig.js
+++ webapp/src/config/poolRoyaleInventoryConfig.js
@@ -279,22 +279,13 @@ const RAW_POOL_ROYALE_HDRI_VARIANTS = [
   },
 ];
 
-const POLYHAVEN_OFFICIAL_HDRI_RESOLUTION_STACK = Object.freeze(['16k', '8k', '4k', '2k', '1k']);
-const POLYHAVEN_OFFICIAL_HDRI_FALLBACK_RESOLUTION = '4k';
-const polyHavenHdriUrl = (assetId, resolution) =>
-  `https://dl.polyhaven.org/file/ph-assets/HDRIs/hdr/${resolution}/${assetId}_${resolution}.hdr`;
+const HDRI_RESOLUTION_STACK = Object.freeze(['2k']);
 
 export const POOL_ROYALE_HDRI_VARIANTS = Object.freeze(
   RAW_POOL_ROYALE_HDRI_VARIANTS.map((variant) => ({
     ...variant,
-    preferredResolutions: POLYHAVEN_OFFICIAL_HDRI_RESOLUTION_STACK,
-    fallbackResolution: POLYHAVEN_OFFICIAL_HDRI_FALLBACK_RESOLUTION,
-    assetUrls: Object.freeze(
-      POLYHAVEN_OFFICIAL_HDRI_RESOLUTION_STACK.reduce((acc, resolution) => {
-        acc[resolution] = polyHavenHdriUrl(variant.assetId, resolution);
-        return acc;
-      }, {})
-    ),
+    preferredResolutions: HDRI_RESOLUTION_STACK,
+    fallbackResolution: HDRI_RESOLUTION_STACK[0],
     thumbnail: polyHavenThumb(variant.assetId),
     ...(POOL_ROYALE_HDRI_PLACEMENTS[variant.id] || {})
   }))
