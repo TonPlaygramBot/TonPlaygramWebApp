3afa9295585b042bc99bbdc9088e649f9adcb269
turdjali <164725460+turdjali@users.noreply.github.com>
2026-05-03 06:06:01 +0200

    Revert "Table Tennis: update thumbnail, add Pool Royale HDRIs to store, add in-game menu, improve AI and audio"

--- webapp/src/config/tennisInventoryConfig.js
+++ webapp/src/config/tennisInventoryConfig.js
@@ -1,19 +1,16 @@
 import { polyHavenThumb } from './storeThumbnails.js';
-import { POOL_ROYALE_HDRI_VARIANTS } from './poolRoyaleInventoryConfig.js';
 
 const reduceLabels = (items) => items.reduce((acc, option) => { acc[option.id] = option.label; return acc; }, {});
 
-export const TENNIS_HDRI_OPTIONS = Object.freeze(
-  POOL_ROYALE_HDRI_VARIANTS.map((variant) => ({
-    id: variant.id,
-    name: variant.name,
-    label: `${variant.name} HDRI`,
-    assetId: variant.assetId,
-    price: Number(variant.price || 0),
-    thumbnail: variant.thumbnail || polyHavenThumb(variant.assetId),
-    type: 'environmentHdri'
-  }))
-);
+export const TENNIS_HDRI_OPTIONS = Object.freeze([
+  { id:'suburbanGarden', name:'Suburban Garden', label:'Suburban Garden HDRI', assetId:'suburban_garden', price: 1900 },
+  { id:'countryTrackMidday', name:'Country Track Midday', label:'Country Track Midday HDRI', assetId:'country_track_midday', price: 1920 },
+  { id:'autumnPark', name:'Autumn Park', label:'Autumn Park HDRI', assetId:'autumn_park', price: 1940 },
+  { id:'rooitouPark', name:'Rooitou Park', label:'Rooitou Park HDRI', assetId:'rooitou_park', price: 1980 },
+  { id:'rotesRathaus', name:'Rotes Rathaus', label:'Rotes Rathaus HDRI', assetId:'rotes_rathaus', price: 2020 },
+  { id:'veniceDawn2', name:'Venice Dawn 2', label:'Venice Dawn 2 HDRI', assetId:'venice_dawn_2', price: 2060 },
+  { id:'piazzaSanMarco', name:'Piazza San Marco', label:'Piazza San Marco HDRI', assetId:'piazza_san_marco', price: 2120 }
+].map((variant) => ({ ...variant, thumbnail: polyHavenThumb(variant.assetId), type: 'environmentHdri' })));
 
 export const TENNIS_DEFAULT_LOADOUT = Object.freeze({ environmentHdri: [TENNIS_HDRI_OPTIONS[0].id] });
 
