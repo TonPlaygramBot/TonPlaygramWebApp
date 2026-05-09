b8380110a17c5e3fa2a398705ba390fc5620c2c5
turdjali <164725460+turdjali@users.noreply.github.com>
2026-05-09 21:10:11 +0200

    Refine Showood pool table visuals

--- webapp/src/config/poolRoyaleTableModels.js
+++ webapp/src/config/poolRoyaleTableModels.js
@@ -33,10 +33,11 @@ export const POOL_ROYALE_TABLE_MODEL_OPTIONS = Object.freeze([
     matchNativeUpperComponentHeight: true,
     useOriginalLayoutSurfaces: true,
     usePoolRoyaleFinish: true,
-    usePoolRoyaleFinishRoles: ['cloth', 'cushion', 'wood', 'pocket'],
-    preserveOriginalSurfaceRoles: ['trim'],
+    usePoolRoyaleFinishRoles: ['cloth', 'cushion', 'wood'],
+    preserveOriginalSurfaceRoles: ['trim', 'pocket'],
     tintOriginalTrimGold: true,
     forceGeneratedChromePlates: false,
+    hideGeneratedPocketCatchers: true,
     hideSurfaceRoles: []
   }
 ]);
