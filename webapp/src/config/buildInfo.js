60221cb4fbec65b8f0ffd53670664ca0d96d95aa
turdjali <164725460+turdjali@users.noreply.github.com>
2026-06-02 07:06:28 +0200

    Revert "Improve store face scan avatar preview"

--- webapp/src/config/buildInfo.js
+++ webapp/src/config/buildInfo.js
@@ -1,2 +1,2 @@
-export const APP_BUILD = "d2aa8b2";
-export const APP_BUILD_GENERATED_AT = "2026-06-02T04:46:47.558Z";
+export const APP_BUILD = "b1afcce";
+export const APP_BUILD_GENERATED_AT = "2026-06-01T05:36:06.214Z";
