8fcc457a6b30e64c93678b2910165f8e0e8646ef
turdjali <164725460+turdjali@users.noreply.github.com>
2026-06-08 08:29:38 +0200

    Revert "Align Pool Royale Showood table mapping"

--- webapp/src/config/poolRoyaleTableModels.js
+++ webapp/src/config/poolRoyaleTableModels.js
@@ -57,7 +57,7 @@ export const POOL_ROYALE_TABLE_MODEL_OPTIONS = Object.freeze([
     useOriginalLayoutSurfaces: true,
     useProceduralBaseWithExternal: false,
     hideOriginalBaseAndLegsForProceduralBase: false,
-    keepGeneratedPocketDropHardware: false,
+    keepGeneratedPocketDropHardware: true,
     usePoolRoyaleFinish: true,
     useReferenceShowoodMapping: true,
     usePoolRoyaleFinishRoles: ['cloth', 'cushion', 'wood'],
