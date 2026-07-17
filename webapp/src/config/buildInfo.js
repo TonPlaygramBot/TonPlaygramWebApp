957010a28ee3845a75196ea82e3096c6e6a19207
turdjali <164725460+turdjali@users.noreply.github.com>
2026-07-17 16:35:02 +0200

    Add protest video gallery

--- webapp/src/config/buildInfo.js
+++ webapp/src/config/buildInfo.js
@@ -1,2 +1,2 @@
-export const APP_BUILD = "d992be4";
-export const APP_BUILD_GENERATED_AT = "2026-06-07T03:14:48.068Z";
+export const APP_BUILD = "467f5ab";
+export const APP_BUILD_GENERATED_AT = "2026-07-17T14:28:10.192Z";
