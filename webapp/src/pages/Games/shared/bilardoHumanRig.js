e7275d2ee7b46548445c25ad32621c894ff2e0c9
turdjali <164725460+turdjali@users.noreply.github.com>
2026-09-13 09:09:16 +0200

    Use realistic cue hand refinement in billiards rig

--- webapp/src/pages/Games/shared/bilardoHumanRig.js
+++ webapp/src/pages/Games/shared/bilardoHumanRig.js
@@ -1,11 +1,19 @@
 import { createHumanRig, chooseHumanEdgePosition, updateHumanPose } from './humanRigCore';
+import { driveHumanPoseWithRealisticHands } from './realisticCueHands';
 
 export function createBilardoHumanRig(scene, opts = {}) {
-  return createHumanRig(scene, opts);
+  return createHumanRig(scene, {
+    cueHands: {
+      bridgeStyle: 'open',
+      gripStyle: 'relaxed',
+      ...(opts.cueHands || {})
+    },
+    ...opts
+  });
 }
 
 export { chooseHumanEdgePosition };
 
 export function updateBilardoHumanPose(human, dt, frameData) {
-  return updateHumanPose(human, dt, frameData);
+  return driveHumanPoseWithRealisticHands(updateHumanPose, human, dt, frameData);
 }
